2c127a66cea19be5e8e16a35895b188d6cbde866
Pham Quoc Trung <trungpham@Phams-MacBook-Air.local>
2024-01-20 10:40:20 +0700

    Fix data form

--- Hand-Invader/app/models/hand_detector/__init__.py
+++ Hand-Invader/app/models/hand_detector/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import torch
 from torch.utils.data import DataLoader
@@ -10,18 +11,19 @@ from image_dataset import ImageDataset
 ROOT_DIR = os.path.dirname(os.path.abspath(__name__)) # This is your Project Root
 
 # data preparation
-IMAGES_DIR = ROOT_DIR+ '/Hand-Invader/app/data/EgoHands/train/images'
-LABELS_DIR = ROOT_DIR+ '/Hand-Invader/app/data/EgoHands/train/labels'
+IMAGES_DIR = ROOT_DIR+ '/Hand-Invader/app/data/EgoHands/train/'
+LABELS_JSON = ROOT_DIR+ '/Hand-Invader/app/data/EgoHands/train/_annotations.coco.json'
 
-image_paths = [os.path.join(IMAGES_DIR, f) for f in os.listdir(IMAGES_DIR) if f.endswith(".jpg")]
-label_paths = [os.path.join(LABELS_DIR, f.replace(".png", ".txt")) for f in os.listdir(LABELS_DIR) if f.endswith(".jpg")]
+with open(LABELS_JSON, "r") as f:
+    coco_data = json.load(f)
 
 transform = transforms.Compose([
     transforms.Resize(256),  # Resize images to 256x256 (adjust as needed)
     transforms.ToTensor(),  # Convert images to PyTorch tensors
 ])
 
-images_dataset = ImageDataset(image_paths=image_paths, label_paths=label_paths, transform=transform)
+images_dataset = ImageDataset(image_dir= IMAGES_DIR, coco_json= coco_data, transform=transform)
 images_dataloader = DataLoader(images_dataset, batch_size=16, shuffle=True) 
 
 
+# model pipeline 
--- Hand-Invader/app/models/hand_detector/image_dataset.py
+++ Hand-Invader/app/models/hand_detector/image_dataset.py
@@ -3,25 +3,29 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class ImageDataset(Dataset):
-    def __init__(self, image_paths, label_paths, transform=None):
-        self.image_paths = image_paths
-        self.label_paths = label_paths
+    def __init__(self, image_dir, coco_json, transform=None):
+        self.image_dir = image_dir
+        self.coco_json = coco_json
         self.transform = transform
+
+        self.image_paths = []
+        for idx, image in enumerate(coco_json['images']):
+            assert idx == image['id']
+            self.image_paths.append(image['files_name'])
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
-        label_path = self.label_paths[idx]
 
         # Read image and label
         image = Image.open(image_path)
-        with open(label_path, "r") as f:
-            lines = f.readlines()
-            label = [[ num for num in line ] for line in lines]
-            label = torch.tensor(label)
-            
+        annotation = self.coco_json['annotations'][idx]
+        
+        assert annotation['id'] == idx
+        label = annotation['bbox']
+
         # Apply transformations (if any)
         if self.transform:
             image = self.transform(image)
