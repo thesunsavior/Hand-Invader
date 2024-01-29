d6ded62134a7981236170187ed154537e514310d
Pham Quoc Trung <trungpham@192.168.1.22>
2024-01-29 21:39:34 +0700

    mdp demo

--- Hand-Invader/app/models/hand_detector/__init__.py
+++ Hand-Invader/app/models/hand_detector/__init__.py
@@ -53,13 +53,24 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__name__)) # This is your Project Roo
 IMAGES_DIR = ROOT_DIR+ '/app/data/EgoHands/train/'
 LABELS_JSON = ROOT_DIR+'/app/data/EgoHands/train/_annotations.coco.json'
 
+IMAGES_VAL_DIR = ROOT_DIR+ '/app/data/EgoHands/valid/'
+LABELS_VAL_JSON = ROOT_DIR+'/app/data/EgoHands/valid/_annotations.coco.json'
+
 with open(LABELS_JSON, "r") as f:
     coco_data = json.load(f)
+
+with open(LABELS_VAL_JSON, "r") as f:
+    coco_valid_data = json.load(f)
 
 
 images_dataset = ImageDataset(image_dir= IMAGES_DIR, coco_json= coco_data, 
                               transform=get_transform())
 images_dataloader = DataLoader(images_dataset, batch_size=32, 
+                               shuffle=False, num_workers=2,collate_fn=collate_fn) 
+
+images_valid_dataset = ImageDataset(image_dir= IMAGES_VAL_DIR, coco_json= coco_valid_data, 
+                              transform=get_transform())
+images_valid_dataloader = DataLoader(images_valid_dataset, batch_size=32, 
                                shuffle=False, num_workers=2,collate_fn=collate_fn) 
 
 hand_detection_model = create_model(num_classes=2, size=300)
