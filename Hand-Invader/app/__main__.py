7e6789607cf0050506cbcbc2b85a7a968b162006
Pham Quoc Trung <trungpham@Phams-Air.lan>
2024-02-01 00:27:17 +0700

    clean up

--- Hand-Invader/app/__main__.py
+++ Hand-Invader/app/__main__.py
@@ -1,11 +1,5 @@
-from app.models.hand_detector import hand_detection_model, images_dataloader, get_transform
+from app.models.hand_detector import hand_detection_model
 from app.util.train_util import train_hand_detection
-from app.util.eval_util import inference, plot_image, plot_loss
-
-import torch
-
-from PIL import Image
-
 
 if __name__ == "__main__":
-    train_hand_detection(hand_detection_model, 5)
+    train_hand_detection(hand_detection_model, 2)
